51022bc069b15c5209698cf59e0e8a03de947900
markherman456@hotmail.com <markherman456@hotmail.com>
2021-11-09 11:23:36 +0000

    Writing Your First Query

--- db.js
+++ db.js
@@ -12,14 +12,15 @@ function handleDisconnect(){
   connection = mysql.createConnection(db_config);
   connection.connect(function(err){
     if(err){
-      console.log('error when connecting to db :', err);
+      console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect,2000);
     }
   });
   connection.on('error',function(err){
-    if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
       handleDisconnect();
-    }else{
+    } else {
       throw err;
     }
   });
